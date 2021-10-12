fb9c244455e1c62245726ee2b38fa3992c9f735e
Sher Muhammad <sherdawar210945@gmail.com>
2021-10-12 14:20:42 +0500

    changes are commited for devconnector

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   mongoURI: 'mongodb://localhost:27017/devconnector',
+//   jwtSecret:'mySecret'
+// };
+
+module.exports = {
+  mongoURI: 'mongodb+srv://sher123:sher123@devconnector.dhcfb.mongodb.net/mern',
+  jwtSecret:'mySecret'
+};
+
+
+
